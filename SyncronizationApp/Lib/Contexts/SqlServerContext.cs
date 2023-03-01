bf736f298113bfd8a1e47ba7614f4a4180666be3
Rustu Koprulu <rustulp@gmail.com>
2023-03-01 10:45:44 -0700

    refactor

--- SyncronizationApp/Lib/Contexts/SqlServerContext.cs
+++ SyncronizationApp/Lib/Contexts/SqlServerContext.cs
@@ -5,7 +5,21 @@ namespace Lib.Contexts
 {
     public class SqlServerContext : DbContext
     {
-        // 4 database tables
+        // NUGET INSTALLS (that should already be installed to this project):
+        // Microsoft.EntityFrameworkCore
+        // Microsoft.EntityFrameworkCore.Design
+        // Microsoft.EntityFrameworkCore.Tools
+        // Microsoft.EntityFrameworkCore.SqlServer (if you're using sql server)
+        // Microsoft.EntityFrameworkCore.Sqlite (if you're using sqlite)
+
+        // TO MAKE CHANGES TO SCHEMA:
+        // enter the following in Package manager console:
+        // 'add-migration' -> will prompt you for a migration name
+        // 'update-database' -> this will update you database
+        //
+        // -Russ
+
+        // 4 db sets (database tables)
         public DbSet<DataEntry> DataEntries { get; set; }
         public DbSet<DeviceType> DeviceTypes { get; set; }
         public DbSet<UnitOfMeasure1> UnitsOfMeasure1 { get; set; }
