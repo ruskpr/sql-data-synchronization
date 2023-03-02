c42608139f6af822d4e2a947cfcc297de70006b1
Rustu Koprulu <rustulp@gmail.com>
2023-03-02 14:22:48 -0700

    updated

--- SyncronizationApp/Lib/Core/Generation/DataGenerator.cs
+++ SyncronizationApp/Lib/Core/Generation/DataGenerator.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Lib.Core.EF.DbContexts;
+using Lib.Core.EF.DbObjects;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +10,45 @@ namespace Lib.Core.Generation
 {
     public class DataGenerator
     {
+        public enum DeviceType
+        {
+            Electric = 1,
+            Gps = 2,
+            Gas = 3,
+            H2O = 4,
+        }
+
+        public enum UOM1Type
+        {
+            kWH = 1,
+            Latitude = 2,
+            CF = 3,
+            CM= 4,
+        }
+
+        public enum UOM2Type
+        {
+            kVA = 1,
+            Longitude = 2,
+            PSI = 3,
+            TEMPCels = 4,
+        }
+
+        public void GenerateData(string path)
+        {
+            SqliteContext db = new SqliteContext();
+
+            for (int i = 0; i < 10000; i++)
+            {
+                var entry = new DataEntry
+                {
+                    DeviceName = "random"
+
+                };
+
+            }
+
+            db.SaveChanges();
+        }
     }
 }
