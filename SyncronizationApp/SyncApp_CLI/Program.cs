c32aafd932b37d0d43cb815a3ee0ac1721e5ea3b
Rustu Koprulu <rustulp@gmail.com>
2023-03-02 13:58:25 -0700

    updated

--- SyncronizationApp/SyncApp_CLI/Program.cs
+++ SyncronizationApp/SyncApp_CLI/Program.cs
@@ -8,9 +8,8 @@ namespace SQL_syncing_service
         public static void Main()
         {
             WindowsNotification.Show(NotiType.ServiceStarted);
-            bool _running = true;
             int delay = AppConfig.SyncIntervalInMilliseconds;
-            while (_running)
+            while (true)
             {
                 // [method to sync data goes here] ex. SyncData()
                 Thread.Sleep(delay);
