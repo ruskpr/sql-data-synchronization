6da89ecb3be0ee5c153c6f34549dd451c4f3680b
Rustu Koprulu <rustulp@gmail.com>
2023-03-03 13:48:04 -0700

    updated

--- SyncronizationApp/Lib/Core/Synchronization/Sync.cs
+++ SyncronizationApp/Lib/Core/Synchronization/Sync.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Lib.Core.EF.DbContexts;
+using Lib.Core.EF.DbObjects;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +10,24 @@ namespace Lib.Core.Synchronization
 {
     public class Sync
     {
+        public static bool IsSynced()
+        {
+            bool isSynced = false;
+            // compare sqlite and sqlserver to see if they are syncronized
+            SqliteContext sqlite = new SqliteContext();
+            SqlServerContext sqlServer = new SqlServerContext();
+
+            List<DataEntry> sqliteRecords = sqlite.DataEntries.ToList();
+            List<DataEntry> sqlServerRecords = sqlServer.DataEntries.ToList();
+
+            //1. loop
+            return isSynced;
+        }
+
+
+        public static void SyncData()
+        {
+
+        }
     }
 }
