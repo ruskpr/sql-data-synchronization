d6f79ff3aee7dcfa1742593be6b4d9682b3c817e
Prathikson <prathiks.jeyakuma@mymhc.ca>
2023-03-03 13:44:29 -0700

    syncapp

--- SyncronizationApp/SyncApp_GUI/Pages/GenerateData.xaml.cs
+++ SyncronizationApp/SyncApp_GUI/Pages/GenerateData.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Lib.Core.EF.DbContexts;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -20,9 +21,11 @@ namespace SyncApp_GUI.Pages
     /// </summary>
     public partial class GenerateData : Page
     {
+        SqliteContext sqlite = new SqliteContext();
         public GenerateData()
         {
             InitializeComponent();
+            dgData.ItemsSource = sqlite.DataEntries.ToList();
         }
 
         private void Button_Click(object sender, RoutedEventArgs e)
